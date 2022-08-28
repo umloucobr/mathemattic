a5f7a914fe762b1255754c9114e3b8c19c3f4ebf
Gabriel Lucas <glucas0609@gmail.com>
2022-08-27 19:17:12 -0700

    Added a functional dropdown menu and completed mainpage :D.

---
+++ source/mainpage/script.js
@@ -0,0 +1,36 @@
+//.value won't work so I had to do it the hard way.
+function myFunction(x) {
+    switch (x) {
+        case 1:
+            window.location.href = "";
+            break;
+
+        case 2:
+            window.location.href = "";
+            break;
+    
+        case 3:
+            window.location.href = "";
+            break;
+            
+        case 4:
+            window.location.href = "";
+            break;  
+
+        case 5:
+            window.location.href = "";
+            break;
+
+        case 6:
+            window.location.href = "";
+            break;
+
+        case 7:
+            window.location.href = "";
+            break;
+
+        case 8:
+            window.location.href = "";
+            break;
+    }
+}
