059aaa42329155fb6dbf32e186b72003afc42ee0
Gabriel Lucas <glucas0609@gmail.com>
2022-09-03 17:25:42 -0700

    Added url parameters and updated fonts and formation

--- source/mainpage/script.js
+++ source/mainpage/script.js
@@ -1,36 +1,37 @@
 //.value won't work so I had to do it the hard way.
+//Sorry for the dumb switch.
 function redirectToExercises(x) {
     switch (x) {
         case 1:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=aritimetica";
             break;
 
         case 2:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=algebra";
             break;
     
         case 3:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=geometriab";
             break;
             
         case 4:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=trigb";
             break;  
 
         case 5:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=estatistica";
             break;
 
         case 6:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=geometriaa";
             break;
 
         case 7:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=triga";
             break;
 
         case 8:
-            window.location.href = "";
+            window.location.href = "/source/exercisepages/exercises.html?id=impossivel";
             break;
     }
 }
